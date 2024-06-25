c9157ce305878fac766573f550447099121351d0
Joel Dias Correia <e_jdcorreia@stefanini.com>
2024-06-25 14:55:31 -0300

    Mejoras SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,13 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="Joel Dias Correia" />
-        <meta name="keywords" content="Joel Dias Correia, Portfolio, UX/UI Designer, Frontend Developer" />
+        <meta name="keywords" content="Joel Dias Correia, Portfolio, UX/UI Designer, Frontend Developer, FullStack Developer" />
         <meta name="theme-color" content="#000000" />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
         <meta name="rating" content="general" />
         <meta name="referrer" content="no-referrer-when-downgrade" />
-        <meta name="description" content="Joel Dias Correia's Portfolio" />
+        <meta name="description" content="Joel Dias Correia's Portfolio - Fullstack developer - Web Development" />
         <GoogleAnalytics/>
       </head>
       <body className={inter.className}>
